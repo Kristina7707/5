0959e31745febb7e9bb25208d3f0969d97c2df6e
Kristina <40335@mail.ru>
2022-08-21 23:03:26 +0300

    add 36

--- 34/Program.cs
+++ 34/Program.cs
@@ -18,7 +18,7 @@ void PrintArray(int[] array)
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i<array.Length-1)Console.Write($"{array[i]},"); 
+        if (i<array.Length-1)Console.Write($"{array[i]}, "); 
         else Console.Write(array[i]); 
         if (array[i] % 2 == 0) even++;
       }   
---
+++ 36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях (индексах).
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int size = 4;
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+     Console.Write("[");
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        if (i<array.Length-1)Console.Write($"{array[i]}, "); 
+        else Console.Write(array[i]);
+    }Console.Write("]");
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    int sum = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+         
+        if (i % 2 > 0) sum += arr[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма чисел, элементов, стоящих на нечётных позициях(индексах) = {sum} ");
+    
+}
+PrintArray(CreateArray(size));
